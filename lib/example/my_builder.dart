533b4c23525c17366037f5e9443fa92c7c2d1ab3
Nikita Solonitsyn <galaxyprise@gmail.com>
2022-03-19 13:11:31 +0500

    master

--- lib/example/my_builder.dart
+++ lib/example/my_builder.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 
-
 class ColoredWidget extends StatefulWidget {
   final Widget child;
   final Color initalColor;
-  
-  const ColoredWidget({ Key? key, required this.child, required this.initalColor }) : super(key: key);
+
+  ColoredWidget({Key? key, required this.child, required this.initalColor})
+      : super(key: key);
   @override
   State<ColoredWidget> createState() => _ColoredWidgetState();
 }
 
 class _ColoredWidgetState extends State<ColoredWidget> {
-  void changeColor(Color color){
-    widget.initalColor = color;
-  }
+  late Color color;
   @override
   void initState() {
     super.initState();
+    color = widget.initalColor;
   }
+
+  void changeColor(Color color) {
+    this.color = color;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -29,19 +33,21 @@ class _ColoredWidgetState extends State<ColoredWidget> {
 
 class MyBuilder extends StatelessWidget {
   const MyBuilder({Key? key}) : super(key: key);
-  void _onPressed(BuildContext context){
-    context.findAncestorStateOfType<_ColoredWidgetState>()?.changeColor(Colors.deepPurpleAccent);
+  void _onPressed(BuildContext context) {
+    context
+        .findAncestorStateOfType<_ColoredWidgetState>()
+        ?.changeColor(Colors.deepPurpleAccent);
   }
+
   @override
   Widget build(BuildContext context) {
     return ColoredWidget(
       initalColor: Colors.blue,
       child: Center(
-        child: Builder(
-          builder: (context) {
-            return ElevatedButton(onPressed: ()=>_onPressed(context), child: Text('ЖМИ'));
-          }
-        ),
+        child: Builder(builder: (context) {
+          return ElevatedButton(
+              onPressed: () => _onPressed(context), child: Text('ЖМИ'));
+        }),
       ),
     );
   }
