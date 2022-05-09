92b925b5e7614acf575803ffe4ff8e5e7d5f68f5
NikitaBerezniki <galaxyprise@gmail.com>
2022-05-10 01:45:06 +0500

    ms3

--- lib/domain/api_client/api_client.dart
+++ lib/domain/api_client/api_client.dart
@@ -40,22 +40,102 @@ class ApiClient {
     return sessionId;
   }
 
+  Future<String> _makeToken() async {
+    final url = _makeUri('/authentication/token/new', {'api_key': _apiKey});
+    final json = await _getMethod(url);
+    return json['request_token'];
+  }
+
+  Future<String> _validateUser(
+      {required String username,
+      required String password,
+      required String requestToken}) async {
+    final url = _makeUri(
+        '/authentication/token/validate_with_login', {'api_key': _apiKey});
+    final parameters = <String, dynamic>{
+      'username': username,
+      'password': password,
+      'request_token': requestToken
+    };
+    
+    final json = await _postMethod(url, parameters);
+    return json['request_token'] as String;
+  }
+
+  Future<String> _makeSession({required String requestToken}) async {
+    final url = _makeUri('/authentication/session/new', {'api_key': _apiKey});
+    final parameters = <String, dynamic>{'request_token': requestToken};
+    final json = await _postMethod(url, parameters);
+    return json['session_id'] as String;
+  }
+
   Future<Map<String, dynamic>> _getMethod(Uri url) async {
-    final request = await _client.getUrl(url);
-    final response = await request.close();
-    final json = _getDataOfResponse(response);
-    return json;
+    try {
+      final request = await _client.getUrl(url);
+      final response = await request.close();
+      final json = _getDataOfResponse(response);
+      return json;
+    } on SocketException {
+      throw ApiClientException(ApiClientExceptionType.Network);
+    } on ApiClientException {
+      rethrow;
+    } catch (e) {
+      throw ApiClientException(ApiClientExceptionType.Other);
+    }
   }
 
   Future<Map<String, dynamic>> _postMethod(
       Uri url, Map<String, dynamic> parameters) async {
-    final request = await _client.postUrl(url);
-    request.headers.contentType = ContentType.json; //аналогично строкой ниже
-    // request.headers.add('Content-type', 'application/json; charset=UTF-8');
-    request.write(jsonEncode(parameters));
-    final response = await request.close();
-    final json = _getDataOfResponse(response);
-    return json;
+    try {
+      final request = await _client.postUrl(url);
+      request.headers.contentType = ContentType.json; //аналогично строкой ниже
+      // request.headers.add('Content-type', 'application/json; charset=UTF-8');
+      request.write(jsonEncode(parameters));
+      final response = await request.close();
+      final json = _getDataOfResponse(response);
+      return json;
+    } on SocketException {
+      throw ApiClientException(ApiClientExceptionType.Network);
+    } on ApiClientException {
+      rethrow;
+    } on AssertionError catch (e) {
+      _errorMessage = e.message.toString();
+      rethrow;
+    } catch (e) {
+      throw ApiClientException(ApiClientExceptionType.Other);
+    }
+  }
+
+  Future<Map<String, dynamic>> _getDataOfResponse(
+      HttpClientResponse response) async {
+    final json = await _responseToJson(response);
+    if (response.statusCode == 200) {
+      _errorMessage = null;
+      return json;
+    } else if (response.statusCode == 401) {
+      final statusCode = json['status_code'];
+      final code = statusCode is int ? statusCode : 0;
+      if (code == 7) {
+        _errorMessage = 'Указан неверный API key.';
+        throw ApiClientException(ApiClientExceptionType.Other);
+      } else if (code == 30) {
+        _errorMessage =
+            'Неправильный имя пользователя/пароль. Пожалуйста, повторите попытку.';
+        throw ApiClientException(ApiClientExceptionType.Auth);
+      } else if (code == 33) {
+        _errorMessage = 'Указан неверный request токен.';
+        throw ApiClientException(ApiClientExceptionType.Other);
+      } else {
+        _errorMessage = json['status_message'];
+        throw ApiClientException(ApiClientExceptionType.Auth);
+      }
+    } else if (response.statusCode == 404) {
+      _errorMessage = 'Запрошенный вами ресурс не удалось найти.';
+      throw ApiClientException(ApiClientExceptionType.Other);
+    } else {
+      _errorMessage = "Статус запроса: ${response.statusCode.toString()}";
+      throw ApiClientException(ApiClientExceptionType.Other);
+    }
   }
 
   Uri _makeUri(String path, [Map<String, dynamic>? parameters]) {
@@ -74,83 +154,6 @@ class ApiClient {
         .toList()
         .then((value) => value.join())
         .then((value) => jsonDecode(value) as Map<String, dynamic>);
-  }
-
-  Future<Map<String, dynamic>> _getDataOfResponse(
-      HttpClientResponse response) async {
-    final json = await _responseToJson(response);
-    if (response.statusCode == 200) {
-      _errorMessage = null;
-      return json;
-    }
-    if (response.statusCode == 401) {
-      if (json['status_code'] == 7) {
-        _errorMessage = 'Указан неверный API key для аунтентификации.';
-        throw ApiClientException(ApiClientExceptionType.Other);
-      } else if (json['status_code'] == 30) {
-        _errorMessage =
-            'Неправильный имя пользователя/пароль. Пожалуйста, повторите попытку.';
-        throw ApiClientException(ApiClientExceptionType.Auth);
-      } else {
-        _errorMessage = json['status_message'];
-        throw ApiClientException(ApiClientExceptionType.Auth);
-      }
-    }
-    if (response.statusCode == 404) {
-      _errorMessage = 'Запрошенный вами ресурс не удалось найти.';
-      throw ApiClientException(ApiClientExceptionType.Other);
-    }
-    _errorMessage = response.statusCode.toString();
-    throw ApiClientException(ApiClientExceptionType.Other);
-  }
-
-  Future<String> _makeToken() async {
-    final url = _makeUri('/authentication/token/new', {'api_key': _apiKey});
-    try {
-      final json = await _getMethod(url);
-      return json['request_token'];
-    } on SocketException {
-      throw ApiClientException(ApiClientExceptionType.Network);
-    } on ApiClientException {
-      rethrow;
-    } catch (e) {
-      throw ApiClientException(ApiClientExceptionType.Other);
-    }
-  }
-
-  Future<String> _validateUser(
-      {required String username,
-      required String password,
-      required String requestToken}) async {
-    final url = _makeUri(
-        '/authentication/token/validate_with_login', {'api_key': _apiKey});
-    final parameters = <String, dynamic>{
-      'username': username,
-      'password': password,
-      'request_token': requestToken
-    };
-    try {
-      assert(requestToken.length == 40 && requestToken.runtimeType == String);
-      final json = await _postMethod(url, parameters);
-      return json['request_token'] as String;
-    } on SocketException {
-      throw ApiClientException(ApiClientExceptionType.Network);
-    } on ApiClientException {
-      rethrow;
-      } on AssertionError catch (e) {
-        _errorMessage = e.message.toString();
-        rethrow;
-    } catch (e) {
-      throw ApiClientException(ApiClientExceptionType.Other);
-    }
-  }
-
-  Future<String> _makeSession({required String requestToken}) async {
-    final url = _makeUri('/authentication/session/new', {'api_key': _apiKey});
-    final parameters = <String, dynamic>{'request_token': requestToken};
-    final json = await _postMethod(url, parameters);
-    if (json['success'] == false) _errorMessage = '${json['status_message']}';
-    return json['session_id'] as String;
   }
 }
 
